e9dc0858056dc5e2d93494cf366f53a2f2689ceb
Juan Diego <juandiegojd252@gmail.com>
2023-06-09 09:26:24 -0500

    Initial commit

---
+++ src/Figuras.java
@@ -0,0 +1,100 @@
+public class Figuras {
+    /* Figuras tridimensionales
+    * 1. Cono
+    * 2. Cilindro
+    * 3. Tetraedro
+    * 4. Piramide
+    * */
+
+    private float altura;
+    private float radio;
+    private float arista;
+    private float base;
+    private float area_base;
+    private float perimetro_base;
+    private float apotema;
+
+    // Constructor
+    public Figuras(float altura, float radio, float arista, float base, float area_base, float perimetro_base, float apotema) {
+        this.altura = altura;
+        this.radio = radio;
+        this.arista = arista;
+        this.base = base;
+        this.area_base = area_base;
+        this.perimetro_base = perimetro_base;
+        this.apotema = apotema;
+    }
+
+    // Getters y setters
+
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getRadio() {
+        return radio;
+    }
+
+    public void setRadio(float radio) {
+        this.radio = radio;
+    }
+
+    public float getArista() {
+        return arista;
+    }
+
+    public void setArista(float arista) {
+        this.arista = arista;
+    }
+
+    public float getBase() {
+        return base;
+    }
+
+    public void setBase(float base) {
+        this.base = base;
+    }
+
+    public float getArea_base() {
+        return area_base;
+    }
+
+    public void setArea_base(float area_base) {
+        this.area_base = area_base;
+    }
+
+    public float getPerimetro_base() {
+        return perimetro_base;
+    }
+
+    public void setPerimetro_base(float perimetro_base) {
+        this.perimetro_base = perimetro_base;
+    }
+
+    public float getApotema() {
+        return apotema;
+    }
+
+    public void setApotema(float apotema) {
+        this.apotema = apotema;
+    }
+
+    private void volumen_cono_(){
+        double volumen_Cono = (1/3)*Math.PI*getRadio()*getRadio()*getAltura();
+    }
+
+    private void area_cono(){
+        double area_cono = Math.PI*getRadio()*(getRadio()+Math.sqrt(getRadio()*getRadio()+getAltura()*getAltura()));
+    }
+    public void imprimir_datos_cono(){
+        System.out.println("El volumen del cono es de:");
+        volumen_cono_();
+        System.out.println("El area del cono es de:");
+        area_cono();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Figuras cono = new Figuras(0,0,0,0,0,0,0);
+        float radio_cono;
+        int opcion = 0;
+        do {
+            System.out.println("---- MENU ----");
+            System.out.println("1. Calcular Volumen y area del Cono");
+            System.out.println("2. Calular volumen y area de la figura 2");
+            System.out.println("3. Calcular volumen y area de la figura 3");
+            System.out.println("4. Calcular volumen y area de la figura 4");
+            System.out.println("5. Salir");
+            System.out.println("Opcion >:");
+            opcion = input.nextInt();
+
+            switch (opcion){
+                case 1:
+                    System.out.println("Ingrese radio:");
+                    radio_cono = input.nextFloat();
+                    cono.setAltura(radio_cono);
+                    cono.imprimir_datos_cono();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    System.out.println("Opcion ingresada de manera incorrecta !");
+                    break;
+            }
+        }while(opcion !=5);
+    }
+}
